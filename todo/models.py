b8ec6e4df26f67d60c711d67c4f0a5422bef9156
Wallacy Braz <wallacy.braz@live.com>
2018-09-19 21:25:08 -0300

    Corrigindo travis

--- todo/models.py
+++ todo/models.py
@@ -4,4 +4,3 @@ from django.db import models
 
 class Todo(models.Model):
     todo_body = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Todo
 
 # Create your tests here.
+
+class TodoTestCase(TestCase):
+    def setUp(self):
+        Todo.objects.create(todo_body='O corpo')
+
+    def test_corpo_criado(self):
+        corpo = Todo.objects.all()[0].todo_body
+        self.assertEqual(corpo,'O corpo')
